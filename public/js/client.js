5803ff21fdee8798eabd2c25f8b4fe4cce260649
andrei.rat <andreirat@ymail.com>
2016-11-27 01:37:33 +0200

    work in progress

--- public/js/client.js
+++ public/js/client.js
@@ -1,33 +1,102 @@
 var app = angular.module('myApp', ['btford.socket-io']).
-    factory('mySocket', function (socketFactory) {
-        return socketFactory();
-    }).
+	factory('mySocket', function (socketFactory) {
+		return socketFactory();
+	}).
 	controller('ArduController', function ($scope,mySocket) {
+		//Led array
 		$scope.ledPins = [
-			{number:0, status:false, color: 'red'},
-			{number:1, status:false, color: 'red' },
-			{number:2, status:false, color: 'red'},
-			{number:3, status:false, color: 'yellowgreen'},
-			{number:4, status:false, color: 'yellowgreen'},
-			{number:5, status:false, color: 'green'},
-			{number:6, status:false, color: 'green'},
-			{number:7, status:false, color: 'green'}
-			];
+			{number:0, led:1, status:false, color: 'red'},
+			{number:1, led:2, status:false, color: 'green' }
+		];
 
-			angular.forEach($scope.ledPins, function(pin){
-				console.log(pin);
-			});
+		//On page load , set motion divs status to false
+		$scope.motion = false;
+		$scope.nomotion = false;
+
+		//Set inital rgb values
+		$scope.redcolor = 0;
+		$scope.greencolor = 0;
+		$scope.bluecolor = 0;
+
+		//Set rgb range values
+		$scope.minrgbval = 1;
+		$scope.maxrgbval = 255;
+
+		//Set each led off
+		$scope.setOff = function () {
+			angular.forEach($scope.ledPins, function (pin) {
+				pin.status = false;
+			})
+		};
+
+		angular.forEach($scope.ledPins, function(pin){
+			console.log(pin);
+		});
 
 		$scope.ledOn = function (p) {
- 			p.status=true;
-	        mySocket.emit('led:on',p);
-	        console.log('Led '+ p.number + ' is on');
-	    };
-    
-	    
-	    $scope.ledOff = function (p) {
-	    	p.status=false;
-	        mySocket.emit('led:off',p);
-	        console.log('Led '+ p.number + ' is off');  
-	    };    
+			p.status=true;
+			mySocket.emit('led:on',p);
+			console.log('Led '+ p.number + ' is on');
+		};
+
+
+		$scope.ledOff = function (p) {
+			p.status=false;
+			mySocket.emit('led:off',p);
+			console.log('Led '+ p.number + ' is off');
+		};
+
+		mySocket.on('motionstart', function (data) {
+			$scope.motion = true;
+			var date = new Date(data);
+			var h = date.getHours();
+			var m = date.getMinutes();
+			var s = date.getSeconds();
+			if(h<9){
+				h = '0'+h;
+			}else if(m<9){
+				m = '0'+m;
+			}else if(s<9){
+				s = '0'+s;
+			}
+			$scope.motionstart =  h+':'+m+':'+s;
+
+				console.log('motionstart', h+':'+m+':'+s);
+		});
+		mySocket.on('motionend', function (data) {
+			$scope.nomotion = true;
+			var date = new Date(data);
+			var h = date.getHours();
+			var m = date.getMinutes();
+			var s = date.getSeconds();
+			if(h<9){
+				h = '0'+h;
+			}else if(m<9){
+				m = '0'+m;
+			}else if(s<9){
+				s = '0'+s;
+			}
+			$scope.motionend =  h+':'+m+':'+s;
+
+			console.log('motionend', data);
+		});
+
+		$scope.setLevelText = function () {
+			var redPercentage = Math.round((($scope.redcolor/255)*100),2);
+			var greenPercentage = Math.round((($scope.greencolor/255)*100),2);
+			var bluePergentage = Math.round((($scope.bluecolor/255)*100),2);
+			angular.element('#redPercentage').html(redPercentage+'%');
+			angular.element('#greenPercentage').html(greenPercentage+'%');
+			angular.element('#bluePercentage').html(bluePergentage+'%');
+
+			$scope.rgb = [
+				{color:'red', value: $scope.redcolor},
+				{color:'green', value: $scope.greencolor},
+				{color:'blue', value: $scope.bluecolor}
+			];
+			mySocket.emit('rgb',$scope.rgb);
+			console.log('Red: '+ $scope.rgb[0].value );
+			console.log('Green: '+ $scope.rgb[1].value );
+			console.log('Blue: '+ $scope.rgb[2].value );
+		};
 });
--- server.js
+++ server.js
@@ -15,6 +15,8 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res) {
         res.sendFile(__dirname + '/public/index.html');
 });
+var sequence = 1;
+var clients = [];
 
 
 httpServer.listen(port);
@@ -25,27 +27,83 @@ console.log('Server available at http://localhost:' + port);
 var led;
 var leds = [];
 var ledPins = [];
-var p;
 
 board.on("ready", function() {
     console.log('Arduino connected');
-    // led = new five.Led(3);
-   ledPins = [2,3,4,5,6,7,8,9];
-   leds = new five.Leds(ledPins);
-});
 
-io.on('connection', function (socket) {
-        console.log(socket.id);
-    var mesaj = {};
+
+    //set rgb pins
+    led = new five.Led.RGB({
+        pins: {
+            red: 3,
+            green: 5,
+            blue: 6
+        }
+    });
+    //set initial state of rgb
+    led.color({
+        red: 0,
+        blue: 0,
+        green: 0
+    });
+    //set LED pins
+    ledPins = [7,8,10,11];
+    leds = new five.Leds(ledPins);
+
+    //Set motion pin
+    var motion = new five.Motion({
+        pin: 2
+    });
+
+    // "calibrated" occurs once, at the beginning of a session,
+    motion.on("calibrated", function() {
+        console.log("calibrated");
+    });
+
+    // "motionstart" events are fired when the "calibrated"
+    // proximal area is disrupted, generally by some form of movement
+    motion.on("motionstart", function() {
+        var date = new Date();
+        io.emit('motionstart', date);
+        leds[2].off();
+        leds[3].on();
+        console.log("motionstart");
+    });
+
+    // "motionend" events are fired following a "motionstart" event
+    // when no movement has occurred in X ms
+    motion.on("motionend", function() {
+        var date = new Date();
+        io.emit('motionend', date);
+        leds[3].fadeOut();
+        leds[2].on();
+        console.log("motionend");
+    });
+});
+    io.on('connection', function (socket) {
+        console.info('New client connected (id=' + socket.id + ').');
+        clients.push(socket);
+
         socket.on('led:on', function (data) {
-            leds[data.number].on();
-           console.log('Led '+data.number+' on');
-        });
+                leds[data.number].on();
+            console.log('Led '+data.number+' on');
+            });
 
         socket.on('led:off', function (data) {
             leds[data.number].off();
             console.log('Led '+data.number+' off');
             
         });
+
+        socket.on('rgb',function(data){
+            led.color({
+                red: data[0].value,
+                blue: data[1].value,
+                green: data[2].value
+            });
+            led.on();
+        })
+
+
     });
 
