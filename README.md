456e78686fb1e750525a35ae8f6edf1f14c497a1
andrei.rat <andreirat@ymail.com>
2016-12-02 23:06:46 +0200

    project structure + improvements

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-My final degree project.
+This project works with Intel Galileo Gen2. It combines multiple data read from sensors and provides an dashboard to which is running on localhost.
+To get started , follow this steps:
+1. ```git clone this repo```
+2. ``cd AngularSocket``
+3. Run `` npm install ``  to install packages.
+4. Run ``node server.js``  to start node server.
+5. Now open browser and go to ``http://localhost:3000``.
+
+Check again for sensor connection info.
--- server.js
+++ server.js
@@ -1,36 +1,49 @@
 // server.js
 var express        = require('express');
 var app            = express();
+var router =       express.Router();
+
 
 var httpServer = require("http").createServer(app);
 var five = require("johnny-five");
+var Oled = require('oled-js');
+var font = require('oled-font-5x7');
 var io=require('socket.io')(httpServer);
-
 var board = new five.Board();
-
-var port = 3000; 
+var port = 3000;
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
-        res.sendFile(__dirname + '/public/index.html');
+    res.sendFile(__dirname + '/public/pages/index.html');
 });
-var sequence = 1;
-var clients = [];
-
-
 httpServer.listen(port);
-
-
 console.log('Server available at http://localhost:' + port);
 
+var clients = [];
 var led;
 var leds = [];
 var ledPins = [];
+var oled;
+
+/*
+ ** @description Options for Oled display SSD1306
+ */
+var opts = {
+    width: 128,
+    height: 64,
+    address: 0x3C
+};
+
 
 board.on("ready", function() {
     console.log('Arduino connected');
 
+
+    oled = new Oled(board, five, opts);
+    oled.turnOnDisplay();
+    oled.clearDisplay(true);
+    oled.update();
 
     //set rgb pins
     led = new five.Led.RGB({
@@ -80,30 +93,50 @@ board.on("ready", function() {
         console.log("motionend");
     });
 });
-    io.on('connection', function (socket) {
-        console.info('New client connected (id=' + socket.id + ').');
-        clients.push(socket);
+io.on('connection', function (socket) {
+    console.info('New client connected (id=' + socket.id + ').');
+    clients.push(socket);
 
-        socket.on('led:on', function (data) {
-                leds[data.number].on();
-            console.log('Led '+data.number+' on');
-            });
+    socket.on('led:on', function (data) {
+        leds[data.number].on();
+        console.log('Led '+data.number+' on');
+    });
 
-        socket.on('led:off', function (data) {
-            leds[data.number].off();
-            console.log('Led '+data.number+' off');
-            
-        });
-
-        socket.on('rgb',function(data){
-            led.color({
-                red: data[0].value,
-                blue: data[1].value,
-                green: data[2].value
-            });
-            led.on();
-        })
-
+    socket.on('led:off', function (data) {
+        leds[data.number].off();
+        console.log('Led '+data.number+' off');
 
     });
 
+    socket.on('rgb',function(data){
+        led.color({
+            red: data[0].value,
+            blue: data[1].value,
+            green: data[2].value
+        });
+        led.on();
+    });
+
+    socket.on('pushText', function (data) {
+        console.log(data);
+        writestring(data);
+    });
+
+    socket.on('clearText', function () {
+        clearDisplay();
+    });
+
+});
+
+function writestring(data){
+    oled.clearDisplay(true);
+    oled.update();
+    oled.setCursor(1, 1);
+    oled.writeString(font, 3, data, 1, true, 2);
+    oled.update();
+}
+
+function clearDisplay(){
+    oled.clearDisplay(true);
+    oled.update();
+}
