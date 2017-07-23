58f0f0a3bc655091185ff43278692692c47bc447
Andrei Rat <andreirat@Andreis-MacBook-Pro.local>
2017-07-23 19:50:01 +0300

    work

--- public/js/client.js
+++ public/js/client.js
@@ -11,117 +11,28 @@ var app = angular.module('myApp', ['btford.socket-io'])
 })
 
 .controller('ArduController', function ($scope,mySocket) {
-	//Led array
-	$scope.ledPins = [
-		{number:0, led:1, status:false, color: 'red'},
-		{number:1, led:2, status:false, color: 'green' }
-	];
 
-	$scope.lcdtext = '';
-	//On page load , set motion divs status to false
-	$scope.motion = false;
-	$scope.nomotion = false;
+    $scope.servo1 = '';
+    $scope.servo2 = '';
 
-	//Set inital rgb values
-	$scope.redcolor = 0;
-	$scope.greencolor = 0;
-	$scope.bluecolor = 0;
+    $scope.servo1Change = function () {
+        console.log($scope.servo1);
+        mySocket.emit('servo:1', $scope.servo1);
+    };
+    $scope.servo2Change = function () {
+        console.log($scope.servo2);
+        mySocket.emit('servo:2', $scope.servo2);
+    };
 
-	//Set rgb range values
-	$scope.minrgbval = 1;
-	$scope.maxrgbval = 255;
+    $scope.sendData = function () {
+        console.log($scope.servo1);
+        console.log($scope.servo2);
+        mySocket.emit('servo:data', $scope.servo1);
 
-	//Set each led off
-	$scope.setOff = function () {
-		angular.forEach($scope.ledPins, function (pin) {
-			pin.status = false;
-		})
-	};
+    };
 
-	angular.forEach($scope.ledPins, function(pin){
-		console.log(pin);
-	});
-
-	$scope.ledOn = function (p) {
-		p.status=true;
-		mySocket.emit('led:on',p);
-		console.log('Led '+ p.number + ' is on');
-	};
-
-
-	$scope.ledOff = function (p) {
-		p.status=false;
-		mySocket.emit('led:off',p);
-		console.log('Led '+ p.number + ' is off');
-	};
-
-	mySocket.on('joystick', function (axis) {
-		$scope.joystickDirectionX = axis.x;
-		$scope.joystickDirectionY = axis.y;
-	});
-
-	mySocket.on('motionstart', function (data) {
-		$scope.motion = true;
-		var date = new Date(data);
-		var h = date.getHours();
-		var m = date.getMinutes();
-		var s = date.getSeconds();
-		if(h<9){
-			h = '0'+h;
-		}else if(m<9){
-			m = '0'+m;
-		}else if(s<9){
-			s = '0'+s;
-		}
-		$scope.motionstart =  h+':'+m+':'+s;
-
-		console.log('motionstart', h+':'+m+':'+s);
-	});
-	mySocket.on('motionend', function (data) {
-		$scope.nomotion = true;
-		var date = new Date(data);
-		var h = date.getHours();
-		var m = date.getMinutes();
-		var s = date.getSeconds();
-		if(h<9){
-			h = '0'+h;
-		}else if(m<9){
-			m = '0'+m;
-		}else if(s<9){
-			s = '0'+s;
-		}
-		$scope.motionend =  h+':'+m+':'+s;
-
-		console.log('motionend', data);
-	});
-
-	$scope.setLevelText = function () {
-		var redPercentage = Math.round((($scope.redcolor/255)*100),2);
-		var greenPercentage = Math.round((($scope.greencolor/255)*100),2);
-		var bluePergentage = Math.round((($scope.bluecolor/255)*100),2);
-		angular.element('#redPercentage').html(redPercentage+'%');
-		angular.element('#greenPercentage').html(greenPercentage+'%');
-		angular.element('#bluePercentage').html(bluePergentage+'%');
-
-		$scope.rgb = [
-			{color:'red', value: $scope.redcolor},
-			{color:'green', value: $scope.greencolor},
-			{color:'blue', value: $scope.bluecolor}
-		];
-		mySocket.emit('rgb',$scope.rgb);
-		console.log('Red: '+ $scope.rgb[0].value );
-		console.log('Green: '+ $scope.rgb[1].value );
-		console.log('Blue: '+ $scope.rgb[2].value );
-	};
-
-	$scope.pushText = function(){
-		console.log($scope.lcdtext);
-		mySocket.emit('pushText', $scope.lcdtext);
-	};
-
-	$scope.clearText = function () {
-		$scope.lcdtext = '';
-		mySocket.emit('clearText');
-	};
+    mySocket.on('hey', function (data) {
+        console.log(data);
+    });
 
 });
--- server.js
+++ server.js
@@ -1,12 +1,8 @@
 // server.js
 var express        = require('express');
 var app            = express();
-var router =       express.Router();
-
-
 var httpServer = require("http").createServer(app);
 var five = require("johnny-five");
-var Oled = require('oled-js');
 var font = require('oled-font-5x7');
 var io=require('socket.io')(httpServer);
 var board = new five.Board();
@@ -22,195 +18,27 @@ httpServer.listen(port);
 console.log('Hello Rat ! Server is runing on port' + port);
 
 
-
-// When board is ready ...
+var servos;
 board.on("ready", function() {
     console.log('Your system is ready. Board id is ' +board.id);
-
-    oled = new Oled(board, five, opts);
-    oled.turnOnDisplay();
-    oled.clearDisplay(true);
-    oled.update();
-
-    // ----------------------- Declarations ------------------ //
-    var oled;
-
-    /*
-     ** @description Options for Oled display SSD1306
-     */
-    var opts = {
-        width: 128,
-        height: 64,
-        address: 0x3C
-    };
-
-    // Create a new `joystick` hardware instance.
-    var joystick = new five.Joystick({
-        pins: ["A0", "A1"]
+    servos = new five.Servos([3, 10]);
+    servos.center();
+    this.repl.inject({
+        servos: servos
     });
 
-    // Light Sensor
-    var light = new five.Light("A0");
-
-    //  Motion Sensor
-    var motion = new five.Motion({
-        pin: 3
-    });
-
-    // RGB pins set-up 
-    var led = new five.Led.RGB({
-        pins: {
-            red: 2,
-            green: 5,
-            blue: 6
-        }
-    });
-
-    // Set LED pins
-    var ledPins = [7,8,10,11];
-    var leds = new five.Leds(ledPins);
-
-
-
-    // --------------------------- Logic ------------------------- //
-
-    // set initial state of RGB
-    led.color({
-        red: 0,
-        green: 0,
-        blue:0
-    });
-
-
-      light.on("change", function() {
-        console.log(this.level);
-      });
-
-
- 
-
-
-
-
-   
-
-
-    // "calibrated" occurs once, at the beginning of a session,
-    motion.on("calibrated", function(data) {
-        console.log(data);
-        console.log("calibrated");
-    });
-
-    // "motionstart" events are fired when the "calibrated"
-    // proximal area is disrupted, generally by some form of movement
-    motion.on("motionstart", function() {
-        var date = new Date();
-        io.emit('motionstart', date);
-        leds[2].off();
-        leds[3].on();
-        console.log("motionstart");
-    });
-
-    // "motionend" events are fired following a "motionstart" event
-    // when no movement has occurred in X ms
-    motion.on("motionend", function() {
-        var date = new Date();
-        io.emit('motionend', date);
-        leds[3].fadeOut();
-        leds[2].on();
-        console.log("motionend");
-    });
-
-
-
-    joystick.on("change", function() {
-        var axis = {
-            x: this.x,
-            y: this.y
-        };
-        if( this.x > 0.5 ){
-            toggle(0);
-        } else if(this.x < -0.5 ){
-            toggle(1)
-        }else if( this.y > 0.5){
-            toggle(2)
-        }else if( this.y < -0.5){
-            toggle(3)
-        }
-
-        io.emit('joystick', axis);
-    });
-
-    function toggle(l){
-        leds.each(function(led, index) {
-            if (index == l ) {
-                led.on();
-            } else {
-                led.off();
-            }
-        });
-    }
 });
 
-// Socket logic 
 io.on('connection', function (socket) {
-
     console.info('New client connected (id=' + socket.id + ').');
-
-    // Led ON action
-    socket.on('led:on', function (data) {
-        leds[data.number].on();
-        console.log('Led '+data.number+' on');
-    });
-
-    // Led OFF action
-    socket.on('led:off', function (data) {
-        leds[data.number].off();
-        console.log('Led '+data.number+' off');
-
-    });
-
-    // RGB action
-    socket.on('rgb',function(data){
+    socket.on('servo:1', function(data) {
         console.log(data);
-        led.color({
-            red: data[0].value,
-            green: data[1].value,
-            blue: data[2].value
-        });
-        led.on();
+        servos[0].to(data);
     });
-
-    // Show text on display action
-    socket.on('pushText', function (data) {
+    socket.on('servo:2', function(data) {
+        servos[1].to(data);
+    });
+    socket.on('servo:data', function(data) {
         console.log(data);
-        writestring(data);
-    });
-    
-
-    /*
-    ** Clear display action
-    */
-    socket.on('clearText', function () {
-        clearDisplay();
     });
 });
-
-/*
-** Function to write data on display
-*/
-function writestring(data){
-    oled.clearDisplay(true);
-    oled.update();
-    oled.setCursor(1, 1);
-    oled.writeString(font, 3, data, 1, true, 2);
-    oled.update();
-}
-
-/*
-**  Function to clear display
-*/
-function clearDisplay(){
-    oled.clearDisplay(true);
-    oled.update();
-}
